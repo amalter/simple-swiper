0b1aa8d19cf3ee3e2837854f1bd1aae4f70f0ee6
Amber Alter <amber@altersego.net>
2023-11-30 13:59:52 -0800

    removed build directory

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c352b34aff88a90fdb54');
--- build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '5c86b911d40cb2565106');
